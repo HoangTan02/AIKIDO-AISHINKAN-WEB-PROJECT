625b21807d79f21d039419c583f0fad1e14db19a
hoangtan16 <hoangtantaccc@gmail.com>
2025-01-22 23:04:08 +0700

    changes location of index.html, fix api links

--- config/loadHeaderFooter.js
+++ config/loadHeaderFooter.js
@@ -1,21 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Tải header
+    fetch("header1.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("header1").innerHTML = data;
+      });
+      
     fetch("header.html")
       .then(response => response.text())
       .then(data => {
         document.getElementById("header").innerHTML = data;
       });
-  
+
     // Tải footer
+    fetch("views/footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer1").innerHTML = data;
+      });
+
     fetch("footer.html")
       .then(response => response.text())
       .then(data => {
         document.getElementById("footer").innerHTML = data;
       });
+
+    fetch("views/media.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("media1").innerHTML = data;
+      });
+
     fetch("media.html")
       .then(response => response.text())
       .then(data => {
         document.getElementById("media").innerHTML = data;
-      });
+      });  
   });
-  
+
